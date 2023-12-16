40a5f7a9bf3b7b44a2873482f6dba2b0a36af4f3
G00406866 <G00406866@atu.ie>
2023-12-16 23:34:17 +0000

    middleware added for req

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const mealRoutes = require('./routes/meals')
 const app = express()
 
 //middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
