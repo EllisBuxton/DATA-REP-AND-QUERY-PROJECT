b9a260aa40818f89cf749322e267ccfc995d6d21
G00406866 <G00406866@atu.ie>
2023-12-17 21:33:40 +0000

    added some data fetch from backend API and added proxy field

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react"
+
 //Homepage component
 const Home = () => {
+    const [meals, setMeals] = useState(null)
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('/api/meals')
+            const json = await response.json()
+
+            if (response.ok) {
+                setMeals(json)
+            }
+        }
+
+        fetchMeals()
+    }, [])
+
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="meals">
+                {meals && meals.map((meal) =>(
+                    <p key={meal._id}>{meal.title}</p>
+                ))}
+                </div> 
             </div>
     )
 }
